0406446022e2592fded4d8b922369e79a1596a02
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-07 14:05:36 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+// Choose a color for an alien as you did in Exercise 25
+let aleinColor = "green";
+// • If the alien’s color is green
+// print a statement that the player just earned 5 points
+if (aleinColor === "green") {
+    console.log("player just earned 5 points!");
+}
+else {
+    console.log("the player just earned 10 points!");
+}
+// • If the alien’s color isn’t green
+//print a statement that the player just earned 10 points.
+aleinColor = "red";
+if (aleinColor === "green") {
+    console.log("player just earned 5 points!");
+}
+else {
+    console.log("the player just earned 10 points!");
+}
